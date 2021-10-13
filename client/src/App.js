962858c39823c5a82bd2f901d17d9a334add1672
Buike369 <chukwubuikekingsley369@gmail.com>
2021-10-13 12:34:30 +0100

    work on EGC

--- client/src/App.js
+++ client/src/App.js
@@ -119,6 +119,8 @@ import Explore_Loans_Page2 from "./components/layout/Explore_Loan/exploreLoanSec
 import Explore_Loans_Page3 from "./components/layout/Explore_Loan/exploreLoanSection10";
 import Explore_Loans_Page4 from "./components/layout/Explore_Loan/exploreLoanSection11";
 import Explore_Loans_Page5 from "./components/layout/Explore_Loan/exploreLoanSection12";
+import EGC from "./components/layout/EGC/egc";
+
 
 const App = () => {
   useEffect(() => {
@@ -223,6 +225,7 @@ const App = () => {
                   <Route exact path="/confirmation" component={Opd} />
                   {/* <Route exact path='/about' component={About} /> */}
                   <Route exact path="/about" component={About} />
+                  <Route exact path="/egc" component={EGC} />
                   <Route exact path="/createloan2" component={Createloan} />
                   <Route exact path="/createloan" component={Createloan2} />
                   <Route
--- client/src/components/layout/Explore_Loan/explore_loans.jsx
+++ client/src/components/layout/Explore_Loan/explore_loans.jsx
@@ -38,6 +38,10 @@ const Explore_loans = ({ loans, fetch }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [categoryChange, setCategoryChange] = useState(false);
     const [statusBtn, setStatus] = useState('');
+    const [generalData, setGeneralData] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(false);
+
+
 
     const handleCategory = (event) => {
         setCategoryChange(true);
@@ -46,7 +50,16 @@ const Explore_loans = ({ loans, fetch }) => {
 
         const arr1 = loanData.filter(d => d.category === event.target.value);
         console.log('arr1', arr1);
-        setMainLoanData(arr1)
+        setMainLoanData(arr1);
+        setGeneralData(arr1);
+
+        if (arr1.length === 0) {
+            setIsEmpty(true);
+            console.log("empty");
+          } else {
+            setIsEmpty(false);
+            console.log("not empty");
+          }
 
     };
 
@@ -55,6 +68,40 @@ const Explore_loans = ({ loans, fetch }) => {
         // var element = document.getElementById(e.currentTarget.id);
         setStatus(e.currentTarget.id)
         // element.classList.add("addBackground");
+
+        if (categoryChange === true) {
+            if (e.currentTarget.id === "Approved") {
+              const uniqueStatus = generalData.filter((d) => d.is_approved === true);
+              console.log("uniqueStatus", uniqueStatus);
+              setMainLoanData(uniqueStatus);
+              if (uniqueStatus.length === 0) {
+                setIsEmpty(true);
+              } else {
+                setIsEmpty(false);
+              }
+            } else if (e.currentTarget.id === "Ongoing") {
+              const uniqueStatus = generalData.filter((d) => d.isConfirmed === true);
+              console.log("uniqueStatus", uniqueStatus);
+              setMainLoanData(uniqueStatus);
+              if (uniqueStatus.length === 0) {
+                setIsEmpty(true);
+              } else {
+                setIsEmpty(false);
+              }
+            }
+          } else {
+            if (e.currentTarget.id === "Approved") {
+              const uniqueStatus = loanData.filter((d) => d.is_approved === true);
+              console.log("uniqueStatus", uniqueStatus);
+              setGeneralData(uniqueStatus);
+            } else if (e.currentTarget.id === "Ongoing") {
+              const uniqueStatus = loanData.filter((d) => d.isConfirmed === true);
+              console.log("uniqueStatus", uniqueStatus);
+              setGeneralData(uniqueStatus);
+            }
+          }
+
+
     }
 
     //  const Roll =[{id:1,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{ id:2,img:'pages.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{ id:3,img:'pages1.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:4,img:'pages2.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:5,img:'pages3.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:6,img:'pages4.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:7,img:'pages5.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:8,img:'pages6.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:9,img:'pages7.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:10,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:11,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:12,img:'pages.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:13,img:'pages1.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:13,img:'pages2.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:14,img:'pages3.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:15,img:'pages4.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:16,img:'pages5.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:17,img:'pages6.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:18,img:'pages7.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:19,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:20,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:21,img:'pages.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:22,img:'pages1.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:23,img:'pages2.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:24,img:'pages3.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:25,img:'pages4.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:26,img:'pages5.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:27,img:'pages6.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:28,img:'pages7.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:29,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:30,img:'pages8.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:31,img:'pages.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:32,img:'pages1.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:34,img:'pages2.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:35,img:'pages3.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:36,img:'pages4.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:37,img:'pages5.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:38,img:'pages6.jpg', price:'#800,000',tag:'RETAIL', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'},{id:39,img:'pages7.jpg', price:'#800,000',tag:'AGRICULTURE', interest:'interest :',interest2:'50% APY', header:'For identity photo studio'}
@@ -67,6 +114,7 @@ const Explore_loans = ({ loans, fetch }) => {
         if (typeof loans.data !== "undefined" && loans.data.length > 0) {
             setIsLoading(false);
             setLoanData(loans.data);
+            setGeneralData(loans.data);
             console.log(loans.data);
 
 
@@ -107,12 +155,12 @@ const Explore_loans = ({ loans, fetch }) => {
                             </div>
 
                         </div>
-                        <div className="calculatorDivCal">
+                        {/* <div className="calculatorDivCal">
                             <div className="interestRangeDivL">
                                 <label className="interestRange">INTEREST RANGE:</label>
                                 <div className="divHab" ><div className="calDiv"><input type="number" placeholder="0" className="calNumber" max="100" /><div className="calInnerDiv"></div><input type="number" placeholder="100" className="calNumber" style={{ appearance: 'text-flied' }} /></div><label className="labPer">%</label></div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
                     <div className="linkDivMin">
@@ -138,8 +186,32 @@ const Explore_loans = ({ loans, fetch }) => {
                 <div className="section2DivHolder_img">
 
                     {
-                        !categoryChange ?
-                            loanData.map((loan, i) => {
+                        !categoryChange ?(
+                            !isEmpty ? (
+
+                            generalData.map((loan, i) => {
+                                return (
+                                    <Fragment>
+                                        <Link to={`loan-details/${loan.id}`} >
+                                            <div className="exploreLoanSection2" key={i}>
+                                                <div className="DivIMG" style={{ backgroundImage: `url(${loan.cover_image})` }}></div>
+
+                                                <p className="retailButton">{loan.category}</p>
+                                                <div className="pTag">$
+                                                <NumberFormat thousandSeparator={true} thousandsGroupStyle="usd" displayType={'text'} value={parseFloat(loan.loan_amount)} />
+
+                                                </div>
+                                                <div className="forIdentity">{loan.title}</div>
+                                                <div><span className="interestSpan">{loan.interest}</span> <span className="percentageSpan">{loan.interest2}</span></div>
+                                            </div>
+                                        </Link>
+                                    </Fragment>
+                                )
+
+                            })
+                            ) :(<div style={{textAlign:'center',margin:'0px auto'}}>Is Empty</div>)
+                            ): !isEmpty ? (
+                             mainLoanData.map((loan, i) => {
                                 return (
                                     <Fragment>
                                         <Link to={`loan-details/${loan.id}`} >
@@ -159,26 +231,14 @@ const Explore_loans = ({ loans, fetch }) => {
                                 )
 
                             }
-                            ) : mainLoanData.map((loan, i) => {
-                                return (
-                                    <Fragment>
-                                        <Link to={`loan-details/${loan.id}`} >
-                                            <div className="exploreLoanSection2" key={i}>
-                                                <div className="DivIMG" style={{ backgroundImage: `url(${loan.cover_image})` }}></div>
-
-                                                <p className="retailButton">{loan.category}</p>
-                                                <div className="pTag">$
-                                                <NumberFormat thousandSeparator={true} thousandsGroupStyle="usd" displayType={'text'} value={parseFloat(loan.loan_amount)} />
-
-                                                </div>
-                                                <div className="forIdentity">{loan.title}</div>
-                                                <div><span className="interestSpan">{loan.interest}</span> <span className="percentageSpan">{loan.interest2}</span></div>
-                                            </div>
-                                        </Link>
-                                    </Fragment>
-                                )
-
-                            }
+                            )
+                            ):(
+                                <div className=" " style={{width:'100%',position:'relative',}}>
+                                    <div  className="" style={{display:'flex',flexDirection:'column',justifyContent:'center',alignContent:'center'}}>
+                                        <div    style={{width:'80%',margin:'0px auto'}}><img src="/img/sorry-image.svg" alt="" style={{width:'50%'}}/></div>
+                                        <div  style={{width:'80%', margin:'0px auto'}}> Oops No data found</div>
+                                    </div>
+                                </div>
                             )
                     }
 
