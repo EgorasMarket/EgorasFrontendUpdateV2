3f249f4d5dadc1b8ce2d58681d289d984011c23e
samuel-2001 <samuelify225@gmail.com>
2021-10-17 20:39:18 +0100

    update

--- client/src/components/layout/Home2/NavBar/Header.js
+++ client/src/components/layout/Home2/NavBar/Header.js
@@ -425,7 +425,7 @@ const Header = () => {
                   id="ArrowDownIcon2"
                   className="ArrowDownIcon"
                 />
-                <ArrowDropUpIcon id="ArrowUpIcon2" className="ArrowUpIcon" />
+                <ArrowDropUpIcon id="ArrowUpIcon2" className="ArrowUpIcon2" />
                 <div className="products-menu menu2" id="products-menu2">
                   <a href="/about" className="drop-borrow-link">
                     About Us
